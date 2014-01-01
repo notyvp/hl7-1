1c5145f15c2fd40db89ba1a6c3b13b157deb7c7d
terrameta <scinnamond@gmail.com>
2014-01-01 18:51:55 -0500

    initial put

---
+++ src/test/java/org/cloudgraph/test/HL7Test.java
@@ -0,0 +1,40 @@
+/**
+ *        CloudGraph Community Edition (CE) License
+ * 
+ * This is a community release of CloudGraph, a dual-license suite of
+ * Service Data Object (SDO) 2.1 services designed for relational and 
+ * big-table style "cloud" databases, such as HBase and others. 
+ * This particular copy of the software is released under the 
+ * version 2 of the GNU General Public License. CloudGraph was developed by 
+ * TerraMeta Software, Inc.
+ * 
+ * Copyright (c) 2013, TerraMeta Software, Inc. All rights reserved.
+ * 
+ * General License information can be found below.
+ * 
+ * This distribution may include materials developed by third
+ * parties. For license and attribution notices for these
+ * materials, please refer to the documentation that accompanies
+ * this distribution (see the "Licenses for Third-Party Components"
+ * appendix) or view the online documentation at 
+ * <http://cloudgraph.org/licenses/>. 
+ */
+package org.cloudgraph.test;
+
+
+
+
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ */
+public abstract class HL7Test extends ExamplesTestCase {
+    private static Log log = LogFactory.getLog(HL7Test.class);
+
+    protected String USERNAME = "hl7";  
+    protected Date now = new Date();
+   
+}
